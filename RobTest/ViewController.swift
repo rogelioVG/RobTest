b8056c26400bf20a39717377c7bd8f84ed847213
rogelioVG <Rogelio@altas-macbook-pro-2.local>
2016-03-12 13:47:36 -0600

    Initial Commit

---
+++ RobTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RobTest
+//
+//  Created by Rogelio Valdes on 3/12/16.
+//  Copyright © 2016 Rogelio Valdes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
